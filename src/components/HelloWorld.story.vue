82d94fe54bd70dde37df7b0879915c17e7e09fb5
Gemiou <efthymiouge@gmail.com>
2023-01-16 12:19:34 +0200

    Initial commit

---
+++ src/components/HelloWorld.story.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './HelloWorld.vue';
+</script>
+
+<template>
+  <Story title="Hello world">
+    <Variant title="default" :initial-state="() => ({})">
+      <template #default="{ state }">
+        <HelloWorld />
+      </template>
+    </Variant>
+  </Story>
+</template>
